e4d9f7759162ad1eb9f79e3297017a5182985adc
Uma993 <ju.umabarathi@gmail.com>
2026-01-22 16:46:53 +0530

    4th mic testing

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ export default function App() {
 
         const volume = dataArray.reduce((a, b) => a + b, 0) / dataArray.length;
 
-        if (volume > 30) {
+        if (volume > 45) {
           setBlown(true);
           audioContext.close();
           return;
